179ad5e81ee85c41301e8113daac5e1b37d80c3a
gksqls <gksqlsdy@gmail.com>
2023-09-21 06:21:34 +0800

    initial commit

--- controller/plugin.go
+++ controller/plugin.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// 木鱼插件
+func Wooden(c *gin.Context) {
+	c.HTML(http.StatusOK, "wooden.tmpl", nil)
+}
+
+// 弹幕插件
 func Barrage(c *gin.Context) {
 	c.HTML(http.StatusOK, "barrage.tmpl", nil)
 }
